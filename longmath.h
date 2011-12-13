5ffefd6ca840c8c2c63d450eed3a7f3bd162b6a1
Alexander Gorodnev <a.gorodnev@gmail.com>
2011-12-13 07:02:27 +0400

    Added correct behavior for operator+

--- longmath.h
+++ longmath.h
@@ -2,6 +2,7 @@
 #define LONGMATH_CPP
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include <cstring>
 
 #define NUMLIMIT 1000
@@ -13,7 +14,8 @@ class	longNumber
     longNumber() { idx = 0; number[0] = 0; }
     longNumber(const longNumber &lN) { idx = lN.idx; memcpy(number, lN.number, idx * sizeof(int)); }
     const longNumber& operator= (const longNumber &lN) { if(this != &lN){ idx = lN.idx; memcpy(number, lN.number, idx * sizeof(int)); } return *this; }
-    const longNumber& operator+=( const longNumber &lN) { for(int i = 0; i < idx; i++) number[i] = number[i] + lN.number[i]; } //wrong operator implementation
+    const longNumber& operator+=(const longNumber &lN) { for(int i = 0; i < idx; i++) number[i] = number[i] + lN.number[i]; } //wrong operator implementation
+    const longNumber operator+(const longNumber &lN) const { longNumber answer; int max_len = max(idx, lN.idx); memset(answer.number, 0, max_len * sizeof(int)); for(int i = 0; i < max_len; i++) { answer.number[i + 1] = (answer.number[i] + number[i] + lN.number[i]) / 1000; answer.number[i] = (answer.number[i] + number[i] + lN.number[i]) % 1000;} if(answer.number[max_len] == 0) answer.idx = max_len; else answer.idx = max_len + 1; return answer; }
     bool read(const string &st);
     bool write(void);
     int get_index(void) {return idx;}
--- main.cpp
+++ main.cpp
@@ -2,12 +2,36 @@
 
 int main()
 {
+#if 0
 	string number;
 	longNumber foo;
 	cout << "Enter a long number:" << endl;
 	getline(cin, number);
 	foo.read(number);
 	foo.write();
+  longNumber baz(foo);
+  baz.write();
+  longNumber bar;
+
+  bar = foo + baz; 
+
+  bar.write();
+#endif
+
+  string number = "0";
+  longNumber a, b, c;
+  a.read(number); 
+  c = a;
+  number = "1";
+  b.read(number); 
+
+  for(int i = 0; i < 110; i++){
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  c.write();
 
 	return 0;
 }
